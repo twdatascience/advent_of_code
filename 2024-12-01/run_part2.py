b41fe23dd3dbe58997813bd27c9e3872f390d929
Tyler Wilson <twtylerwilson@gmail.com>
2024-12-12 11:26:05 -0700

    adjust file names and part 2

--- 2024-12-01/run_part2.py
+++ 2024-12-01/run_part2.py
@@ -16,12 +16,18 @@ with open('2024-12-01/input.txt', 'r') as file:
 column1_int = [int(x) for x in column1]
 column2_int = [int(x) for x in column2]
 
-column1_int.sort()
-column2_int.sort()
+# count number of times each element in column1 appears in column2
+counts = {}
+for x in column1_int:
+    x_count = column2_int.count(x)
+    if x_count > 0:
+        counts[x] = column2_int.count(x)
 
-# compare the two lists by element and save diff
-diff = [abs(int(x) - int(y)) for x, y in zip(column1_int, column2_int)]
+# calculate similiarity score 
+similarity_score = 0
 
-total_sum = sum(diff)
+for key, value in counts.items():
+    sim_score = key * value
+    similarity_score += sim_score
 
-print(total_sum)  # Output the total sum of the differences
+print(similarity_score)
